27c2386aeca8a005b5435cbb81668a6f87e1e3b5
rkanatza <rkanatza@gmail.com>
2016-08-24 12:22:59 -0500

    added variables

--- kanatzarrobinaca/app/src/main/java/com/robinkanatzar/android/aca/marsrobot/MarsRobot.java
+++ kanatzarrobinaca/app/src/main/java/com/robinkanatzar/android/aca/marsrobot/MarsRobot.java
@@ -4,4 +4,9 @@ package com.robinkanatzar.android.aca.marsrobot;
  * Created by robinkanatzar on 8/24/16.
  */
 public class MarsRobot {
+    String status;
+    int speed;
+    float temperature;
+
+
 }
