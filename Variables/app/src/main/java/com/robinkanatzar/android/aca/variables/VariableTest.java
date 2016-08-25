d9ee1cb193503c323e0be225cdfc86a9c322f296
rkanatza <rkanatza@gmail.com>
2016-08-25 12:23:28 -0500

    created new class feedback file and changed java on hw

--- Variables/app/src/main/java/com/robinkanatzar/android/aca/variables/VariableTest.java
+++ Variables/app/src/main/java/com/robinkanatzar/android/aca/variables/VariableTest.java
@@ -10,6 +10,7 @@ public class VariableTest extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_variable_test);
 
+        //declare variables
         String rstring;
         float rfloat;
         int rint;
@@ -18,6 +19,18 @@ public class VariableTest extends AppCompatActivity {
         char rchar;
         short rshort;
         byte rbyte;
-        
+
+        //assign values to each variable
+
+        //print out the values of each variable
+        System.out.println("string: " + rstring);
+        System.out.println("float: " + rfloat);
+        System.out.println("int: " + rint);
+        System.out.println("double: " + rdouble);
+        System.out.println("boolean: " + rbool);
+        System.out.println("char: " + rchar);
+        System.out.println("short: " + rshort);
+        System.out.println("byte: " + rbyte);
+
     }
 }
