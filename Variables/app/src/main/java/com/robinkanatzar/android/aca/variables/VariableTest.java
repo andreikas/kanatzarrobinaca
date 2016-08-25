3555a3855b4cb01cc2787dc94b32772fd6221d65
rkanatza <rkanatza@gmail.com>
2016-08-25 12:09:16 -0500

    added new variables project

---
+++ Variables/app/src/main/java/com/robinkanatzar/android/aca/variables/VariableTest.java
@@ -0,0 +1,13 @@
+package com.robinkanatzar.android.aca.variables;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VariableTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_variable_test);
+    }
+}
