6aba648f9e94f2da5df98cbeb8c655e17bd14d5c
Robin Kanatzar <robinkanatzar@Robins-MacBook-Pro.local>
2016-08-30 09:21:23 -0500

    added test button

--- FindingFrench/app/src/main/java/com/robinkanatzar/android/aca/findingfrench/MainActivity.java
+++ FindingFrench/app/src/main/java/com/robinkanatzar/android/aca/findingfrench/MainActivity.java
@@ -1,13 +1,31 @@
 package com.robinkanatzar.android.aca.findingfrench;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // call the function to set the button click listener
+        setButtonClickListener();
+
+    }
+
+    private void setButtonClickListener() {
+        // set the button click listener
+
+        Button testButton = (Button) findViewById(R.id.testButton);
+        testButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+
+            }
+        });
     }
 }
