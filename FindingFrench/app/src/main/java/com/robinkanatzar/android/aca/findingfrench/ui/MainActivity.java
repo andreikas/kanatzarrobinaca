07514519bb7818ad0f4d4aac35ea5fe7206aafa5
Robin Kanatzar <rkanatza@gmail.com>
2016-09-08 15:14:58 -0500

    Started smells like bakin project

--- FindingFrench/app/src/main/java/com/robinkanatzar/android/aca/findingfrench/ui/MainActivity.java
+++ FindingFrench/app/src/main/java/com/robinkanatzar/android/aca/findingfrench/ui/MainActivity.java
@@ -1,9 +1,11 @@
-package com.robinkanatzar.android.aca.findingfrench;
+package com.robinkanatzar.android.aca.findingfrench.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+
+import com.robinkanatzar.android.aca.findingfrench.R;
 
 public class MainActivity extends AppCompatActivity {
 
--- Santa/app/src/main/java/com/robinkanatzar/android/aca/santa/SantaActivity.java
+++ Santa/app/src/main/java/com/robinkanatzar/android/aca/santa/SantaActivity.java
@@ -1,7 +1,10 @@
 package com.robinkanatzar.android.aca.santa;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class SantaActivity extends AppCompatActivity {
 
@@ -9,5 +12,36 @@ public class SantaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_santa);
+    }
+
+    // for buttons, onClick = processClicks
+    public void processClicks(View display) {
+        //Log.d("RCK", "View = " + display);
+        // display returns id/imageButton and a long string before that
+
+        Intent action = null;
+        int id = display.getId(); // returns id of button clicked (parses the long display value above)
+            //imageButton1, 2, 3
+
+        switch (id) {
+            case(R.id.imageButton):
+                // this is the dialer button
+
+                // Intent (action to take, data associated with action)
+                action = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:877-446-6723"));
+                break;
+            case(R.id.imageButton2):
+                // this is the browse button
+                action = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.noradsanta.org"));
+                break;
+            case(R.id.imageButton3):
+                // this is the map button
+                action = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=101 Saint Nicholas Dr.," + " North Pole, AK"));
+                break;
+            default:
+                break;
+        }
+
+        startActivity(action); // uses intent to start the next activity
     }
 }
