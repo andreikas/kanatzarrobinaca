b877b7d619b8fd1e21033297ab98286c4d79e34a
Robin Kanatzar <rkanatza@gmail.com>
2016-09-09 08:49:56 -0500

    udpated class feedback and worked on SmellslikeBakin

---
+++ SmellsLikeBakin/app/src/main/java/com/robinkanatzar/android/aca/smellslikebakin/ListFragment.java
@@ -0,0 +1,26 @@
+package com.robinkanatzar.android.aca.smellslikebakin;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ListFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_list, container, false);
+
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.listRecyclerView);
+        ListAdapter listAdapter = new ListAdapter();
+        recyclerView.setAdapter(listAdapter);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+
+        return view;
+    }
+}
--- SmellsLikeBakin/app/src/main/java/com/robinkanatzar/android/aca/smellslikebakin/MainActivity.java
+++ SmellsLikeBakin/app/src/main/java/com/robinkanatzar/android/aca/smellslikebakin/MainActivity.java
@@ -1,7 +1,8 @@
 package com.robinkanatzar.android.aca.smellslikebakin;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListFragment fragment = new ListFragment();
+        android.app.FragmentManager fragmentManager = getFragmentManager();
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.placeholder, fragment);
+        fragmentTransaction.commit();
     }
 }
