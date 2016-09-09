bd643aad474bcf5747bbf4b70ba1a37020cdeb9f
Robin Kanatzar <rkanatza@gmail.com>
2016-09-09 10:03:47 -0500

    started Real UI project

--- SmellsLikeBakin/app/src/main/java/com/robinkanatzar/android/aca/smellslikebakin/ListFragment.java
+++ SmellsLikeBakin/app/src/main/java/com/robinkanatzar/android/aca/smellslikebakin/ListFragment.java
@@ -10,9 +10,16 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class ListFragment extends Fragment {
+
+    public interface OnRecipeSelectedInterface {
+        void onListRecipeSelected(int index);
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        OnRecipeSelectedInterface listener = (OnRecipeSelectedInterface) getActivity();
+
         View view = inflater.inflate(R.layout.fragment_list, container, false);
 
         RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.listRecyclerView);
