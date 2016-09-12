da0fb784fce73380caf653e334cdecb8760a50b4
Robin Kanatzar <rkanatza@gmail.com>
2016-09-12 09:36:16 -0500

    finished alien ship project

--- AccessScopeThisAndStatic/app/src/main/java/com/robinkanatzar/android/aca/accessscopethisandstatic/MainActivity.java
+++ AccessScopeThisAndStatic/app/src/main/java/com/robinkanatzar/android/aca/accessscopethisandstatic/MainActivity.java
@@ -1,7 +1,8 @@
 package com.robinkanatzar.android.aca.accessscopethisandstatic;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AlienShip girlShip = new AlienShip();
+        AlienShip boyShip = new AlienShip();
+
+        girlShip.shipName = "Corrine Yu";
+        boyShip.shipName = "Andre LaMothe";
+
+        Log.i("girlShip strngth: ", "" + girlShip.getShieldStrength());
+
+        Log.i("boyShip shieldStrngth: ", "" + boyShip.getShieldStrength());
+
+        boyShip.hitDetected();
+        boyShip.hitDetected();
+        boyShip.hitDetected();
+
+        Log.i("girlShip Strngth: ", "" + girlShip.getShieldStrength());
+
+        Log.i("boyShip shieldStrngth: ", "" + boyShip.getShieldStrength());
+
+        boyShip.hitDetected(); //ahhh
+
+        Log.i("girlShip Strngth: ", "" + girlShip.getShieldStrength());
+
+        Log.i("boyShip shieldStrngth: ", "" + boyShip.getShieldStrength());
+        Log.i("numShips: ", "" +    AlienShip.getNumShips());
+
+
+
     }
 }
