ba921f5a60dd558876f80fb9cf89aee8987d1e11
Robin Kanatzar <rkanatza@gmail.com>
2016-09-27 16:03:53 -0500

    made more changes to note to self

---
+++ Notetoself/app/src/main/java/com/robinkanatzar/android/aca/notetoself/DialogEditNote.java
@@ -0,0 +1,79 @@
+package com.robinkanatzar.android.aca.notetoself;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+public class DialogEditNote extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.dialog_edit_note, null);
+
+        // TODO: change all to match layout and names on dialog_edit_note
+        final EditText editTitle = (EditText) dialogView.findViewById(R.id.editTitle);
+        final EditText editDescription = (EditText) dialogView.findViewById(R.id.editDescription);
+        final CheckBox checkBoxIdea = (CheckBox) dialogView.findViewById(R.id.checkBoxIdea);
+        final CheckBox checkBoxTodo = (CheckBox) dialogView.findViewById(R.id.checkBoxTodo);
+        final CheckBox checkBoxImportant = (CheckBox) dialogView.findViewById(R.id.checkBoxImportant);
+        Button btnCancel = (Button) dialogView.findViewById(R.id.btnCancel);
+        Button btnOK = (Button) dialogView.findViewById(R.id.btnOK);
+        Button btnChangePic = (Button) dialogView.findViewById(R.id.buttonAddPic);
+        ImageView imageViewPreview = (ImageView) dialogView.findViewById(R.id.imageViewPicConfirm);
+
+        builder.setView(dialogView);
+
+        // Handle the cancel button
+        btnCancel.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+
+        // Handle the OK button
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //TODO change this so it's editing an existing note
+                // Create a new note
+                Note newNote = new Note();
+
+                // Set its variables to match the users entries on the form
+                newNote.setTitle(editTitle.getText().toString());
+                newNote.setDescription(editDescription.getText().toString());
+                newNote.setIdea(checkBoxIdea.isChecked());
+                newNote.setTodo(checkBoxTodo.isChecked());
+                newNote.setImportant(checkBoxImportant.isChecked());
+                //newNote.setImage(photo);
+
+                // Get a reference to MainActivity
+                MainActivity callingActivity = (MainActivity) getActivity();
+
+                // Pass newNote back to MainActivity
+                callingActivity.createNewNote(newNote);
+
+                // Quit the dialog
+                dismiss();
+            }
+        });
+
+
+        // TODO handle button to change picture
+
+        return builder.create();
+
+    }
+}
