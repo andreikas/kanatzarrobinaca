893c3e79ad5790ea689869899e27fb55022f2f00
Robin Kanatzar <rkanatza@gmail.com>
2016-10-13 09:44:19 -0500

    added rx example app

---
+++ DetectNetworkExample/app/src/main/java/com/robinkanatzar/android/rck/detectnetworkexample/MainActivity.java
@@ -0,0 +1,50 @@
+package com.robinkanatzar.android.rck.detectnetworkexample;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button mButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mButton = (Button) findViewById(R.id.button);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (haveNetworkConnection()) {
+                    Toast.makeText(MainActivity.this, "Connection true", Toast.LENGTH_SHORT).show();
+                } else if (!haveNetworkConnection()) {
+                    Toast.makeText(MainActivity.this, "No connection, false", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private boolean haveNetworkConnection() {
+        boolean haveConnectedWifi = false;
+        boolean haveConnectedMobile = false;
+
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo[] netInfo = cm.getAllNetworkInfo();
+        for (NetworkInfo ni : netInfo) {
+            if (ni.getTypeName().equalsIgnoreCase("WIFI"))
+                if (ni.isConnected())
+                    haveConnectedWifi = true;
+            if (ni.getTypeName().equalsIgnoreCase("MOBILE"))
+                if (ni.isConnected())
+                    haveConnectedMobile = true;
+        }
+        return haveConnectedWifi || haveConnectedMobile;
+    }
+}
