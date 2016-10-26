8edb4fc090caf5c1fca472c10f694c56bd6cc4b9
Robin Kanatzar <rkanatza@gmail.com>
2016-10-26 10:24:10 -0500

    finished stormy list app from treehouse

--- StormyLists/app/src/main/java/teamtreehouse/com/stormy/weather/Hour.java
+++ StormyLists/app/src/main/java/teamtreehouse/com/stormy/weather/Hour.java
@@ -3,6 +3,9 @@ package teamtreehouse.com.stormy.weather;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by benjakuben on 2/5/15.
  */
@@ -31,8 +34,10 @@ public class Hour implements Parcelable {
         mSummary = summary;
     }
 
-    public double getTemperature() {
-        return mTemperature;
+    public int getTemperature() {
+
+
+        return (int) Math.round(mTemperature);
     }
 
     public void setTemperature(double temperature) {
@@ -41,6 +46,10 @@ public class Hour implements Parcelable {
 
     public String getIcon() {
         return mIcon;
+    }
+
+    public int getIconId() {
+        return Forecast.getIconId(mIcon);
     }
 
     public void setIcon(String icon) {
@@ -53,6 +62,12 @@ public class Hour implements Parcelable {
 
     public void setTimezone(String timezone) {
         mTimezone = timezone;
+    }
+
+    public String getHour() {
+        SimpleDateFormat formatter = new SimpleDateFormat("h a");
+        Date date = new Date (mTime * 1000);
+        return formatter.format(date);
     }
 
     @Override
@@ -88,6 +103,8 @@ public class Hour implements Parcelable {
             return new Hour[size];
         }
     };
+
+
 }
 
 
